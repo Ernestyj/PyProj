587c9b77e2058a1672f884c7798e695309e5a84c
ernestyj@outlook.com <ernestyj@outlook.com>
2015-09-16 14:06:26 +0800

    c

--- test.py
+++ test.py
@@ -1,24 +1,7 @@
-import bottle
-import bottle_mysql
+__author__ = 'Eugene'
 
-app = bottle.Bottle()
-# dbhost is optional, default is localhost
-plugin = bottle_mysql.Plugin(dbuser='root', dbpass='root', dbname='community_server', dbhost='202.120.40.24',
-                             dictrows=False)
-plugin2 = bottle_mysql.Plugin(dbuser='root', dbpass='root', dbname='community_server', dbhost='202.120.40.24',
-                             dictrows=False, keyword='rb')
+x = 1
 
-app.install(plugin)
-app.install(plugin2)
-
-
-@app.route('/show/<item>', mysql={'dbname': 'community_server'})
-def show(item, rb):
-    print('hi')
-    rb.execute('select * from user')
-    row = rb.fetchone()
-    if row:
-        return bottle.template('<b>Hello {{name}}</b>!', name=row)
-    return HTTPError(404, "Page not found")
-
-app.run(host='localhost', port=8080)
+print(vars())
+print(globals())
+print(locals())
