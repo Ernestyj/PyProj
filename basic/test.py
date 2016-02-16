970f6cea1791b59b94cf3d2c7c8d944c12ca017a
Eugene <ernestyj@outlook.com>
2016-02-16 22:37:44 +0800

    basic py grammer

---
+++ basic/test.py
@@ -0,0 +1,11 @@
+import sys
+
+__author__ = 'Eugene'
+
+list=['xaa', 'xzz', 'axx', 'bbb', 'ccc']
+list.sort()
+print list
+list2=['xaa', 'xzz', 'axx', 'bbb', 'ccc']
+print list.extend(list2)
+s="s"
+"s".lower()
