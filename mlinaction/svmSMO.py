b535d176257f7dd32f4a62ca65160468eab3ef33
Eugene <ernestyj@outlook.com>
2016-03-10 11:52:48 +0800

    Platt smo

--- mlinaction/svmSMO.py
+++ mlinaction/svmSMO.py
@@ -17,7 +17,7 @@ def loadDataSet(fileName=os.getcwd()+'/data/testSet_svm.txt'):
         labelMat.append(float(lineArr[2]))
     return dataMat,labelMat
 
-'''
+'''简化版SMO*********************************************************************************
 SMO算法中的外循环确定要优化的最佳alpha对。
 而简化版却会跳过这一部分，首先在数据集上遍历每一个alpha,
 然后在剩下的alpha集合中随机选择另一个alpha ，从而构建alpha对.
@@ -116,4 +116,178 @@ def test_1():
     for i in range(len(dataArrList)):
         if alphas[i]>0.0: print dataArrList[i], labelArrList[i]
 
-test_1()
+# test_1()
+'''**********************************************************************************'''
+
+'''Platt SMO数据结构
+'''
+class optStruct:
+    def __init__(self,dataMatIn, classLabels, C, toler, kTup):  # Initialize the structure with the parameters
+        self.X = dataMatIn
+        self.labelMat = classLabels
+        self.C = C
+        self.tol = toler
+        self.m = shape(dataMatIn)[0]
+        self.alphas = mat(zeros((self.m,1)))
+        self.b = 0
+        # eCache的第一列给出的是eCache是否有效的标志位，而第二列给出的是实际的E值。
+        self.eCache = mat(zeros((self.m,2))) #first column is valid flag
+        self.K = mat(zeros((self.m,self.m)))
+        for i in range(self.m):
+            self.K[:,i] = kernelTrans(self.X, self.X[i,:], kTup)
+
+'''计算E值
+'''
+def calcEk(optStruc, k):
+    fXk = float(multiply(optStruc.alphas, optStruc.labelMat).T * optStruc.K[:, k] + optStruc.b)
+    Ek = fXk - float(optStruc.labelMat[k])
+    return Ek
+
+'''于选择第二个alpha(内循环的alpha值)(启发式)
+目标是选择合适的第二个alpha值以保证在每次优化中采用最大步长
+'''
+def selectJ(i, optStruc, Ei):         #this is the second choice -heurstic, and calcs Ej
+    maxK = -1; maxDeltaE = 0; Ej = 0
+    optStruc.eCache[i] = [1, Ei]  #set valid #choose the alpha that gives the maximum delta E
+    validEcacheList = nonzero(optStruc.eCache[:, 0].A)[0]
+    if (len(validEcacheList)) > 1:
+        for k in validEcacheList:   #loop through valid Ecache values and find the one that maximizes delta E
+            if k == i: continue #don't calc for i, waste of time
+            Ek = calcEk(optStruc, k)
+            deltaE = abs(Ei - Ek)
+            if (deltaE > maxDeltaE):
+                maxK = k; maxDeltaE = deltaE; Ej = Ek
+        return maxK, Ej
+    else:   #in this case (first time around) we don't have any valid eCache values
+        j = selectJrand(i, optStruc.m)
+        Ej = calcEk(optStruc, j)
+    return j, Ej
+
+'''计算误差值并存人缓存
+'''
+def updateEk(optStruc, k):#after any alpha has changed update the new value in the cache
+    Ek = calcEk(optStruc, k)
+    optStruc.eCache[k] = [1, Ek]
+
+'''完整Platt SMO算法的优化例程(内层),类似smoSimple
+'''
+def innerL(i, oS):
+    Ei = calcEk(oS, i)
+    if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or ((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
+        j,Ej = selectJ(i, oS, Ei) #this has been changed from selectJrand
+        alphaIold = oS.alphas[i].copy(); alphaJold = oS.alphas[j].copy();
+        if (oS.labelMat[i] != oS.labelMat[j]):
+            L = max(0, oS.alphas[j] - oS.alphas[i])
+            H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+        else:
+            L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+            H = min(oS.C, oS.alphas[j] + oS.alphas[i])
+        if L==H: print "L==H"; return 0
+        eta = 2.0 * oS.K[i, j] - oS.K[i, i] - oS.K[j, j] #changed for kernel
+        if eta >= 0: print "eta>=0"; return 0
+        oS.alphas[j] -= oS.labelMat[j] * (Ei - Ej) / eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j], H, L)
+        updateEk(oS, j) #added this for the Ecache
+        if (abs(oS.alphas[j] - alphaJold) < 0.00001): print "j not moving enough"; return 0
+        oS.alphas[i] += oS.labelMat[j] * oS.labelMat[i] * (alphaJold - oS.alphas[j])#update i by the same amount as j
+        updateEk(oS, i) #added this for the Ecache                    #the update is in the oppostie direction
+        b1 = oS.b - Ei - oS.labelMat[i] * (oS.alphas[i] - alphaIold) * oS.K[i, i] - oS.labelMat[j] * (oS.alphas[j] - alphaJold) * oS.K[i, j]
+        b2 = oS.b - Ej - oS.labelMat[i] * (oS.alphas[i] - alphaIold) * oS.K[i, j] - oS.labelMat[j] * (oS.alphas[j] - alphaJold) * oS.K[j, j]
+        if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]): oS.b = b1
+        elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]): oS.b = b2
+        else: oS.b = (b1 + b2) / 2.0
+        return 1
+    else: return 0
+
+'''完整Platt SMO算法的外循环
+输入和函数smoSimple完全一样
+'''
+def smoP(dataMatIn, classLabels, C, toler, maxIter,kTup=('lin', 0)):    #full Platt SMO
+    oS = optStruct(mat(dataMatIn),mat(classLabels).transpose(),C,toler, kTup)
+    iter = 0
+    entireSet = True; alphaPairsChanged = 0
+    while (iter < maxIter) and ((alphaPairsChanged > 0) or (entireSet)):
+        alphaPairsChanged = 0
+        if entireSet:   #go over all
+            for i in range(oS.m):
+                alphaPairsChanged += innerL(i,oS)
+                print "fullSet, iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged)
+            iter += 1
+        else:#go over non-bound (railed) alphas
+            nonBoundIs = nonzero((oS.alphas.A > 0) * (oS.alphas.A < C))[0]
+            for i in nonBoundIs:
+                alphaPairsChanged += innerL(i,oS)
+                print "non-bound, iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged)
+            iter += 1
+        if entireSet: entireSet = False #toggle entire set loop
+        elif (alphaPairsChanged == 0): entireSet = True
+        print "iteration number: %d" % iter
+    return oS.b,oS.alphas
+
+'''计算w向量
+'''
+def calcWs(alphas,dataArr,classLabels):
+    X = mat(dataArr); labelMat = mat(classLabels).transpose()
+    m,n = shape(X)
+    w = zeros((n,1))
+    for i in range(m):
+        w += multiply(alphas[i]*labelMat[i],X[i,:].T)
+    return w
+
+'''径向基核函数
+'''
+def kernelTrans(X, A, kTup): #calc the kernel or transform data to a higher dimensional space
+    m,n = shape(X)
+    K = mat(zeros((m,1)))
+    if kTup[0]=='lin': K = X * A.T   #linear kernel
+    elif kTup[0]=='rbf':
+        for j in range(m):
+            deltaRow = X[j,:] - A
+            K[j] = deltaRow*deltaRow.T
+        K = exp(K/(-1*kTup[1]**2)) #divide in NumPy is element-wise not matrix like Matlab
+    else: raise NameError('Houston We Have a Problem -- That Kernel is not recognized')
+    return K
+
+def test_2():
+    dataArrList, labelArrList = loadDataSet()
+    b, alphas = smoP(dataArrList, labelArrList, C=0.6, toler=0.001, maxIter=40)
+    print b
+    print alphas[alphas>0]
+    print '支持向量有:'
+    for i in range(len(dataArrList)):
+        if alphas[i]>0.0: print dataArrList[i], labelArrList[i]
+
+# test_2()
+
+'''如果降低σ(K1)，那么训练错误率就会降低，但是测试错误率却会上升。
+
+支持向量的数目存在一个最优值。SVM的优点在于它能对数据进行高效分类。
+如果支持向量太少，就可能会得到一个很差的决策边界（下个例子会说明这一点）；
+如果支持向量太多，也就相当于每次都利用整个数据集进行分类，这种分类方法称为k近邻。
+'''
+def test_Rbf(k1=1.3):
+    dataArrList,labelArrList = loadDataSet(os.getcwd()+'/data/testSetRBF.txt')
+    b,alphas = smoP(dataArrList, labelArrList, 200, 0.0001, 10000, ('rbf', k1)) #C=200 important
+    datMat=mat(dataArrList); labelMat = mat(labelArrList).transpose()
+    svInd=nonzero(alphas.A>0)[0]
+    sVs=datMat[svInd] #get matrix of only support vectors
+    labelSV = labelMat[svInd];
+    print "there are %d Support Vectors" % shape(sVs)[0]
+    m,n = shape(datMat)
+    errorCount = 0
+    for i in range(m):
+        kernelEval = kernelTrans(sVs,datMat[i,:],('rbf', k1))
+        predict=kernelEval.T * multiply(labelSV,alphas[svInd]) + b
+        if sign(predict)!=sign(labelArrList[i]): errorCount += 1
+    print "the training error rate is: %f" % (float(errorCount)/m)
+    dataArrList,labelArrList = loadDataSet(os.getcwd()+'/data/testSetRBF2.txt')
+    errorCount = 0
+    datMat=mat(dataArrList); labelMat = mat(labelArrList).transpose()
+    m,n = shape(datMat)
+    for i in range(m):
+        kernelEval = kernelTrans(sVs,datMat[i,:],('rbf', k1))
+        predict=kernelEval.T * multiply(labelSV,alphas[svInd]) + b
+        if sign(predict)!=sign(labelArrList[i]): errorCount += 1
+    print "the test error rate is: %f" % (float(errorCount)/m)
+
+test_Rbf()
